8af0bfacd399fad2e7ed193beaf454ef3dbd279a
Adam Zimmer <ok@dsc.com>
2019-03-27 14:36:55 -0700

    first commit - hosted on rebellion-landing-2

---
+++ src/landing/landing.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './landing.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faTwitterSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
+
+const icon = <FontAwesomeIcon icon={faFacebook} />
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <div class='landing-content'>
+          <div class='social-icons'>
+            <FontAwesomeIcon class='fa-icon' icon={faFacebook} />
+            <FontAwesomeIcon class='fa-icon' icon={faTwitterSquare} />
+            <FontAwesomeIcon class='fa-icon' icon={faInstagram} />
+          </div>
+          <div class='title-container'>
+            <div class='title'>
+              <h2>
+              Rebellion
+              </h2>
+              <h1>
+                COMING SOON
+              </h1>
+              Want an interest free loan? <a href='#'> Let us know!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class='landing-container'>
+          <div id="picture"></div>
+          <div id="layer1"></div>
+          <div id="layer2"></div>
+          <div id="layer3"></div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;

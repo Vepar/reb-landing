7efb72014b9ac6b4bb7354b60c46f870ed47664f
Adam Zimmer <ok@dsc.com>
2019-03-27 22:53:49 -0700

    deleting bootstrap

--- src/landing/landing.jsx
+++ src/landing/landing.jsx
@@ -4,8 +4,30 @@ import RButton from '../shared/components/r-button';
 import './landing.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitterSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 class Landing extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
       <div>
@@ -23,7 +45,7 @@ class Landing extends Component {
               <h1>
                 COMING SOON
               </h1>
-                Want an interest free loan? <a href='#'> Let us know!</a>
+                Want an interest free loan? <a href='#' onClick={this.handleShow}> Let us know!</a>
               {/* <div class='subscribe-container'> */}
                 {/* <div class="input-group mb-3">
                   <input type="text" class="form-control" placeholder="Email" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
@@ -39,6 +61,22 @@ class Landing extends Component {
           <div id="picture"></div>
           <div id="background"></div>
         </div>
+
+
+        <Modal show={this.state.show} onHide={this.handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
